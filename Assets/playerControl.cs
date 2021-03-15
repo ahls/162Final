b71fc45cdf581c870dc7cd81ab415b594fafb87d
Ahlon Sin <ahls@uci.edu>
2021-03-15 02:32:37 -0700

    added gravity and materials

--- Assets/playerControl.cs
+++ Assets/playerControl.cs
@@ -6,10 +6,16 @@ public class playerControl : MonoBehaviour
 {
     [SerializeField] Camera Cam;
     Transform CamTransform;
+    public Transform groundCheck;
     CharacterController CC;
-    public float speed;
     Animator Anim;
+    public float speed;
+    public float groundDistance = 0.01f;
+    public LayerMask groundMask;
+    public float gravity = 9.8f;
+    public Vector3 yVelo = Vector3.zero;
     bool isMoving;
+    bool onGround;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,12 +29,28 @@ public class playerControl : MonoBehaviour
     {
 
         transform.eulerAngles = new Vector3(0, Cam.GetComponent<cameraControl>().yaw, 0);
+        onGround = Physics.CheckSphere(transform.position, groundDistance, groundMask);
+        if (!onGround)
+        {
+            yVelo.y -= gravity;
+        }
+        else
+        {
+            Debug.Log("floor hit");
+            yVelo = Vector3.zero;
+        }
+
     }
     private void FixedUpdate()
     {
-        CC.Move(transform.forward*Input.GetAxis("Vertical")*Time.deltaTime*speed);
-        CC.Move(transform.right * Input.GetAxis("Horizontal") * Time.deltaTime);
-        isMoving = (Input.GetAxis("Vertical") + Input.GetAxis("Horizontal") != 0);
+        
+        float vertAxis = Input.GetAxis("Vertical");
+        float horAxis = Input.GetAxis("Horizontal");
+
+        CC.Move(transform.forward*vertAxis*Time.deltaTime*speed);
+        CC.Move(transform.right * horAxis * Time.deltaTime*speed);
+        CC.Move(yVelo*Time.deltaTime);
+        isMoving = (vertAxis  != 0 || horAxis != 0);
 
         Anim.SetBool("moving", isMoving);
     }
