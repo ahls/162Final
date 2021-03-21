9632f3217302f47549deec2337fb90219312321d
Ahlon Sin <ahls@uci.edu>
2021-03-20 17:11:52 -0700

    made more change

---
+++ Assets/returnToTitle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class returnToTitle : MonoBehaviour
+{
+    float Timer = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+        if(Timer <= 0)
+        {
+            SceneManager.LoadScene("Title Screen");
+        }
+    }
+}
